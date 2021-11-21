010d527e81a418ddefc708055072ac09a9ff4b48
intellisenseCodez <oyekanmiakande@gmail.com>
2021-11-21 23:09:14 +0100

    add stattic_root

--- HMS/HMS/settings.py
+++ HMS/HMS/settings.py
@@ -122,15 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/assets/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
